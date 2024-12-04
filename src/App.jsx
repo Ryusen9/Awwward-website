f8b67842f71744eab44f005075f317544af640e2
Ryusen9 <asratul02@gmail.com>
2024-12-04 20:03:54 +0600

    Created hero components and halfway towards completing it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Hero from "./Components/Hero"
 
 function App() {
 
   return (
-    <>
-            
-    </>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+       <Hero/>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,22 @@ export default {
         zentry: ["zentry", "sans-serif"],
         general: ["general", "sans-serif"],
       },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      },
     },
   },
   plugins: [],
