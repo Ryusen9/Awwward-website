83e0aec68a72b95573efcd34fe19275eb82b266a
Ryusen9 <asratul02@gmail.com>
2024-12-04 17:39:18 +0600

    Added fonts and cofigured it into the project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'circular-web': ["circular-web", "sans-serif"],
+        'robert-medium' : ["robert-medium", "sans-serif"],
+        'robert-regular' : ["robert-regular", "sans-serif"],
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
