3ee07f63c1e27d690c5b67b0e149fbbe6809a762
Ryusen9 <asratul02@gmail.com>
2024-12-10 23:44:26 +0600

    Styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <main className="relative min-h-screen overflow-x-hidden">
+    <main className="relative min-h-screen overflow-x-hidden bg-zinc-500">
       <Navbar />
       <Hero />
       <About />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,29 @@
+import { useRef } from "react";
+import navImage from "/public/img/logo.png"
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Navbar = () => {
+  const navRef = useRef(null);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div
+      ref={navRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2 ">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src={navImage} alt="logo" className="w-10"/>
+            <Button id="product-button" title="Products" rightIcon={<TiLocationArrow/>} containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"/>
+          </div>
+          <div className="flex h-full items-center">
+            <div className="hidden md:block"></div>
 
-export default Navbar
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
