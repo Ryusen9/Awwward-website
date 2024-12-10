44131907c4b987d9f00761c721d964df50458f7c
Ryusen9 <asratul02@gmail.com>
2024-12-11 00:40:08 +0600

    Navbar design halfway.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { useRef } from "react";
-import navImage from "/public/img/logo.png"
+import navImage from "/public/img/logo.png";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 
 const Navbar = () => {
+  const menuItems = ["Home", "Nexus", "About", "Support"];
   const navRef = useRef(null);
+  const toggleAudioIndecator = () => {};
+  const audioElementRef = useRef(null);
   return (
     <div
       ref={navRef}
@@ -13,13 +16,43 @@ const Navbar = () => {
       <header className="absolute top-1/2 w-full -translate-y-1/2 ">
         <nav className="flex size-full items-center justify-between p-4">
           <div className="flex items-center gap-7">
-            <img src={navImage} alt="logo" className="w-10"/>
-            <Button id="product-button" title="Products" rightIcon={<TiLocationArrow/>} containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"/>
+            <img src={navImage} alt="logo" className="w-10" />
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
           </div>
           <div className="flex h-full items-center">
             <div className="hidden md:block"></div>
-
+            {menuItems.map((item) => (
+              <a
+                href={`#${item.toLocaleLowerCase()}`}
+                key={Math.random()}
+                className="nav-hover-btn"
+              >
+                {item}
+              </a>
+            ))}
           </div>
+          <button
+            className="ml-10 flex items-center space-x-0.5"
+            onClick={toggleAudioIndecator}
+          >
+            <audio
+              src="/public/audio/loop.mp3"
+              ref={audioElementRef}
+              className="hidden"
+              loop
+            >
+              {[1, 2, 3, 4, 5].map(bar => (
+                <div key={bar} className={`indicator-line`}>
+
+                </div>
+              ))}
+            </audio>
+          </button>
         </nav>
       </header>
     </div>
